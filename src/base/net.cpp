7c49d05f48185a4d4d47fd90310c43fa302ba3dd
Redix <redix@hotmail.de>
2026-08-23 00:14:24 +0200

    Hand the websocket transport to the network instead of linking it in
    The lowest layer opened websockets by calling into the engine, so every
    program that links the shared engine had to bring libwebsockets with it,
    whether it ever touches a socket or not. Since the network half of the
    engine was split off, the sixteen tools that only read a map and the demo
    render tool no longer link that half, and with websockets enabled they
    stopped linking at all: the socket code they carry references seven
    functions nobody gives them.
    
    The transport is now a table of function pointers that the lowest layer is
    handed, and libwebsockets installs it by being linked. Nothing below the
    engine includes anything from it any more, the websocket branches cost a
    null check instead of a build option, and a program without the transport
    simply cannot open a websocket address, which is exactly what a program
    built without websockets could do before.
    
    (cherry picked from commit 6cb54dd43f4a29cd98fc01623242be2e68ac815d)

--- src/base/net.cpp
+++ src/base/net.cpp
@@ -13,10 +13,6 @@
 #include <iterator> // std::size
 #include <string_view>
 
-#if defined(CONF_WEBSOCKETS)
-#include <engine/shared/websockets.h>
-#endif
-
 #if defined(CONF_PLATFORM_EMSCRIPTEN)
 #include <emscripten.h>
 #endif
@@ -99,7 +95,6 @@ static void net_buffer_reinit(NETSOCKET_BUFFER *buffer)
 }
 #endif
 
-#if defined(CONF_WEBSOCKETS)
 static void net_buffer_simple(NETSOCKET_BUFFER *buffer, char **buf, int *size)
 {
 #if defined(CONF_PLATFORM_LINUX)
@@ -110,7 +105,6 @@ static void net_buffer_simple(NETSOCKET_BUFFER *buffer, char **buf, int *size)
 	*size = sizeof(buffer->buf);
 #endif
 }
-#endif
 
 struct NETSOCKET_INTERNAL
 {
@@ -124,6 +118,8 @@ struct NETSOCKET_INTERNAL
 	NETSOCKET_BUFFER buffer;
 };
 static NETSOCKET_INTERNAL invalid_socket = {NETTYPE_INVALID, -1, -1, -1, -1, false};
+
+static const NETWEBSOCKET *websocket_transport = nullptr;
 
 const NETADDR NETADDR_ZEROED = {NETTYPE_INVALID, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0};
 
@@ -797,9 +793,11 @@ void net_init()
 	WSADATA wsa_data;
 	dbg_assert(WSAStartup(MAKEWORD(1, 1), &wsa_data) == 0, "WSAStartup failure");
 #endif
-#if defined(CONF_WEBSOCKETS)
-	websocket_init();
-#endif
+}
+
+void net_websocket_transport(const NETWEBSOCKET *transport)
+{
+	websocket_transport = transport;
 }
 
 int net_errno()
@@ -898,16 +896,14 @@ int net_socket_read_wait(NETSOCKET sock, std::chrono::nanoseconds nanoseconds)
 		FD_SET(sock->ipv6sock, &readfds);
 		maxfd = std::max(maxfd, sock->ipv6sock);
 	}
-#if defined(CONF_WEBSOCKETS)
 	if(sock->web_ipv4sock >= 0)
 	{
-		maxfd = std::max(maxfd, websocket_fd_set(sock->web_ipv4sock, &readfds));
+		maxfd = std::max(maxfd, websocket_transport->fd_set(sock->web_ipv4sock, &readfds));
 	}
 	if(sock->web_ipv6sock >= 0)
 	{
-		maxfd = std::max(maxfd, websocket_fd_set(sock->web_ipv6sock, &readfds));
+		maxfd = std::max(maxfd, websocket_transport->fd_set(sock->web_ipv6sock, &readfds));
 	}
-#endif
 	if(maxfd < 0)
 	{
 		return 0;
@@ -927,16 +923,14 @@ int net_socket_read_wait(NETSOCKET sock, std::chrono::nanoseconds nanoseconds)
 	{
 		return 1;
 	}
-#if defined(CONF_WEBSOCKETS)
-	if(sock->web_ipv4sock >= 0 && websocket_fd_get(sock->web_ipv4sock, &readfds))
+	if(sock->web_ipv4sock >= 0 && websocket_transport->fd_get(sock->web_ipv4sock, &readfds))
 	{
 		return 1;
 	}
-	if(sock->web_ipv6sock >= 0 && websocket_fd_get(sock->web_ipv6sock, &readfds))
+	if(sock->web_ipv6sock >= 0 && websocket_transport->fd_get(sock->web_ipv6sock, &readfds))
 	{
 		return 1;
 	}
-#endif
 	return 0;
 }
 
@@ -958,14 +952,12 @@ static void priv_net_close_all_sockets(NETSOCKET sock)
 		sock->type &= ~NETTYPE_IPV4;
 	}
 
-#if defined(CONF_WEBSOCKETS)
 	if(sock->web_ipv4sock >= 0)
 	{
-		websocket_destroy(sock->web_ipv4sock);
+		websocket_transport->destroy(sock->web_ipv4sock);
 		sock->web_ipv4sock = -1;
 		sock->type &= ~NETTYPE_WEBSOCKET_IPV4;
 	}
-#endif
 
 	if(sock->ipv6sock >= 0)
 	{
@@ -974,14 +966,12 @@ static void priv_net_close_all_sockets(NETSOCKET sock)
 		sock->type &= ~NETTYPE_IPV6;
 	}
 
-#if defined(CONF_WEBSOCKETS)
 	if(sock->web_ipv6sock >= 0)
 	{
-		websocket_destroy(sock->web_ipv6sock);
+		websocket_transport->destroy(sock->web_ipv6sock);
 		sock->web_ipv6sock = -1;
 		sock->type &= ~NETTYPE_WEBSOCKET_IPV6;
 	}
-#endif
 
 	free(sock);
 }
@@ -1091,19 +1081,17 @@ NETSOCKET net_udp_create(NETADDR bindaddr)
 		}
 	}
 
-#if defined(CONF_WEBSOCKETS)
-	if(bindaddr.type & NETTYPE_WEBSOCKET_IPV4)
+	if(websocket_transport != nullptr && (bindaddr.type & NETTYPE_WEBSOCKET_IPV4))
 	{
 		NETADDR bindaddr_websocket_ipv4 = bindaddr;
 		bindaddr_websocket_ipv4.type = NETTYPE_WEBSOCKET_IPV4;
-		const int socket = websocket_create(&bindaddr_websocket_ipv4);
+		const int socket = websocket_transport->create(&bindaddr_websocket_ipv4);
 		if(socket >= 0)
 		{
 			sock->type |= NETTYPE_WEBSOCKET_IPV4;
 			sock->web_ipv4sock = socket;
 		}
 	}
-#endif
 
 	if(bindaddr.type & NETTYPE_IPV6)
 	{
@@ -1138,19 +1126,17 @@ NETSOCKET net_udp_create(NETADDR bindaddr)
 		}
 	}
 
-#if defined(CONF_WEBSOCKETS)
-	if(bindaddr.type & NETTYPE_WEBSOCKET_IPV6)
+	if(websocket_transport != nullptr && (bindaddr.type & NETTYPE_WEBSOCKET_IPV6))
 	{
 		NETADDR bindaddr_websocket_ipv6 = bindaddr;
 		bindaddr_websocket_ipv6.type = NETTYPE_WEBSOCKET_IPV6;
-		const int socket = websocket_create(&bindaddr_websocket_ipv6);
+		const int socket = websocket_transport->create(&bindaddr_websocket_ipv6);
 		if(socket >= 0)
 		{
 			sock->type |= NETTYPE_WEBSOCKET_IPV6;
 			sock->web_ipv6sock = socket;
 		}
 	}
-#endif
 
 	if(sock->type == NETTYPE_INVALID)
 	{
@@ -1213,7 +1199,6 @@ int net_udp_send(NETSOCKET sock, const NETADDR *addr, const void *data, int size
 		}
 	}
 
-#if defined(CONF_WEBSOCKETS)
 	if(addr->type & NETTYPE_WEBSOCKET_IPV4)
 	{
 		if(sock->web_ipv4sock >= 0)
@@ -1224,7 +1209,7 @@ int net_udp_send(NETSOCKET sock, const NETADDR *addr, const void *data, int size
 			}
 			else
 			{
-				d = websocket_send(sock->web_ipv4sock, (const unsigned char *)data, size, addr);
+				d = websocket_transport->send(sock->web_ipv4sock, (const unsigned char *)data, size, addr);
 			}
 		}
 		else
@@ -1232,7 +1217,6 @@ int net_udp_send(NETSOCKET sock, const NETADDR *addr, const void *data, int size
 			log_error("net", "Cannot send Websocket IPv4 traffic to this socket");
 		}
 	}
-#endif
 
 	if(addr->type & NETTYPE_IPV6)
 	{
@@ -1265,7 +1249,6 @@ int net_udp_send(NETSOCKET sock, const NETADDR *addr, const void *data, int size
 		}
 	}
 
-#if defined(CONF_WEBSOCKETS)
 	if(addr->type & NETTYPE_WEBSOCKET_IPV6)
 	{
 		if(sock->web_ipv6sock >= 0)
@@ -1276,7 +1259,7 @@ int net_udp_send(NETSOCKET sock, const NETADDR *addr, const void *data, int size
 			}
 			else
 			{
-				d = websocket_send(sock->web_ipv6sock, (const unsigned char *)data, size, addr);
+				d = websocket_transport->send(sock->web_ipv6sock, (const unsigned char *)data, size, addr);
 			}
 		}
 		else
@@ -1284,7 +1267,6 @@ int net_udp_send(NETSOCKET sock, const NETADDR *addr, const void *data, int size
 			log_error("net", "Cannot send Websocket IPv6 traffic to this socket");
 		}
 	}
-#endif
 
 	network_stats.sent_bytes += size;
 	network_stats.sent_packets++;
@@ -1369,13 +1351,12 @@ int net_udp_recv(NETSOCKET sock, NETADDR *addr, unsigned char **data)
 	}
 #endif
 
-#if defined(CONF_WEBSOCKETS)
 	if(sock->web_ipv4sock >= 0)
 	{
 		char *buf;
 		int size;
 		net_buffer_simple(&sock->buffer, &buf, &size);
-		bytes = websocket_recv(sock->web_ipv4sock, (unsigned char *)buf, size, addr);
+		bytes = websocket_transport->recv(sock->web_ipv4sock, (unsigned char *)buf, size, addr);
 		*data = (unsigned char *)buf;
 		if(bytes > 0)
 		{
@@ -1389,7 +1370,7 @@ int net_udp_recv(NETSOCKET sock, NETADDR *addr, unsigned char **data)
 		char *buf;
 		int size;
 		net_buffer_simple(&sock->buffer, &buf, &size);
-		bytes = websocket_recv(sock->web_ipv6sock, (unsigned char *)buf, size, addr);
+		bytes = websocket_transport->recv(sock->web_ipv6sock, (unsigned char *)buf, size, addr);
 		*data = (unsigned char *)buf;
 		if(bytes > 0)
 		{
@@ -1397,7 +1378,6 @@ int net_udp_recv(NETSOCKET sock, NETADDR *addr, unsigned char **data)
 			return bytes;
 		}
 	}
-#endif
 
 	return bytes < 0 ? -1 : 0;
 }
